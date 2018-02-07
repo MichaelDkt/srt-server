d26bc1c5be9f039a67bb9a6e8db2d6f0e1b393b2
Dominique BUREAU <dominique.bureau@decathlon.com>
2018-02-07 15:30:06 +0100

    deleteAddress

--- queries/deleteAddress.js
+++ queries/deleteAddress.js
@@ -1,24 +1,44 @@
 const { Client } = require('pg');
-const client = new Client();
-
 const getReserveId = require("./getReserveId");
 
 function deleteAddress(store, address){
 
+  const client = new Client();
   client.connect();
 
+  let reserve_id;
 
   return getReserveId(store)
+
+    // check if there are some items in this address before to delete it.
     .then(reserve_id => {
-      return client.query("UPDATE addresses SET disabled = true WHERE id = $1 AND reserve_id = $2",[address, reserve_id]);
+      return client.query("SELECT count(*) FROM stock_addresses WHERE address_id = $1", [address])
     })
+
+    // if there are some items in this address, no delete.
     .then(result => {
-      client.end();
-      return({
-        code: "200",
-        text: "OK"
-      });
+
+      if (result.rows[0].count === '0'){
+
+        return client.query("UPDATE addresses SET disabled = true WHERE id = $1",[address])
+        .then( result => {
+          client.end();
+          return({
+            code: "200",
+            text: "OK"
+          });
+        });
+
+      } else {
+
+        return({
+          code: "400",
+          text: "There are some items in this address. Impossible to delete it."
+        });
+
+      }
     })
+
     .catch(error => {
       console.warn(error);
       client.end();
@@ -30,4 +50,4 @@ function deleteAddress(store, address){
   })
 }
 
-module.exports = createAddress;
+module.exports = deleteAddress;
