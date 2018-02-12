d08ea3758b7edb0e09761ef9e14da41ce39e61dc
Dominique BUREAU <dominique.bureau@decathlon.com>
2018-02-12 16:53:21 +0100

    working progress

--- queries/getAllAddresses.js
+++ queries/getAllAddresses.js
@@ -7,7 +7,8 @@ function getAllAddresses(store, address){
     .then(reserve_id => {
       client.connect();
       return client.query("\
-      SELECT a.id, a.address, a.disabled, a.reserve_id, count(sa.id) AS qty_ref  \
+      SELECT a.id, a.address, a.disabled, a.reserve_id, count(sa.id) AS qty_ref, \
+      CASE WHEN SUM(sa.qty) ISNULL THEN 0 ELSE SUM(sa.qty) END AS stock_total  \
       FROM addresses AS a LEFT JOIN stock_addresses AS sa ON sa.address_id = a.id \
       WHERE reserve_id = $1 \
       GROUP BY a.id, a.address, a.disabled, a.reserve_id \
